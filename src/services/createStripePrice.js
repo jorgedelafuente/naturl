7a17e44b76d0c59f9ebe1be99f29ce254caf9021
Sofiya Lazarova <sofiya@sofiya.ca>
2020-07-02 13:42:27 +0200

    style: fix style in imports

--- src/services/createStripePrice.js
+++ src/services/createStripePrice.js
@@ -1,11 +1,14 @@
-// import mockData from './mockData.json';
 const mockData = require('./mockData.json');
-// const mockDataObject = JSON.parse(mockData);
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_CHECKOUT_SK);
+const stripe = require('stripe')('YOUR_STRIPE_SK_HERE');
 const fs = require('fs');
 
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
 const addToStripe = (data) => {
-  return data.map(async (item) => {
+  return data.map(async (item, i) => {
+    await sleep(1000/10*i);
     const stripeProduct = await stripe.products.create({
       name: item.name,
       description: item.description,
@@ -14,18 +17,24 @@ const addToStripe = (data) => {
     });
     const stripePrice = await stripe.prices.create({
       product: stripeProduct.id,
-      unit_amount: item.price * 100,
+      unit_amount: (item.price * 100).toFixed(0),
       currency: 'cad',
     });
-    return { ...item, stripe_price: stripePrice.id };
+    return {
+      ...item,
+      stripe_product_id: stripeProduct.id,
+      stripe_price_id: stripePrice.id,
+    };
   });
 };
 
-Promise.all(addToStripe(mockData)).then((dataWithStripe) => {
-  fs.writeFile('./mockDataWithStripe.json', dataWithStripe, function (err) {
-    if (err) {
-      throw err;
-    }
-    console.log('The file was saved!');
-  });
-});
+Promise.all(addToStripe(mockData))
+  .then((data) => {
+    fs.writeFile('./mockDataWithStripe.json', JSON.stringify(data), function(err) {
+      if (err) {
+        throw err;
+      }
+      console.log('The file was saved!');
+    });
+  })
+  .catch((error) => console.log(error));
