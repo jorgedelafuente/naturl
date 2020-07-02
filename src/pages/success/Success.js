4749473582a5951a7618e7ef16f574d495fa27df
Sofiya Lazarova <sofiya@sofiya.ca>
2020-07-02 15:36:39 +0200

    style: add style and text to success page

--- src/pages/success/Success.js
+++ src/pages/success/Success.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Success.scss';
 
 const Success = () => {
-  return <div>Success Page</div>;
+  return (
+    <div className="Success-container">
+      <h2>Thanks for shopping at NATURL</h2>
+      <div className="Success-message">Your order is on it&apos;s way!</div>
+    </div>
+  );
 };
 
 export default Success;
