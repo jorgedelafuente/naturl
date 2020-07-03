45aae9fdeac0d858e8a23490b6b846588adfe3e9
Jorge de La Fuente <jorge.a.delafuente@gmail.com>
2020-07-03 12:53:44 +0200

    fix: fixed all eslint warnings

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -4,7 +4,8 @@ import './productItem.scss';
 import ProductItem from './ProductItem';
 
 const Product = (props) => {
-  const foundItem = props.data.find((item) => item.id == props.id);
+  console.log(props);
+  const foundItem = props.data.find((item) => item.id === parseInt(props.id));
   return (
     <div className="product-page-container">
       <ProductItem data={foundItem} />
